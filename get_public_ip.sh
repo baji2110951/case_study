756e18233592e23dce7a4c12fb7afe3b2f4a6b6e
baji2110951 <61617189+baji2110951@users.noreply.github.com>
2023-09-17 22:52:02 +0530

    Update get_public_ip.sh

--- get_public_ip.sh
+++ get_public_ip.sh
@@ -7,21 +7,8 @@ PUBLIC_IP=$(terraform output -raw ec2_public_ip)
 echo $INSTANCE_ID
 echo $PUBLIC_IP
 
-echo PUBLIC_IP
-
 echo "Assign public Ip In Ansible host file"
-
-pwd
 
 cd ../config
 
-pwd
-
 sed -i "s/PUBLIC_IP_1/$PUBLIC_IP/g" hosts
-
-echo "sed is done"
-
-ssh ec2-user@$PUBLIC_IP
-ls
-
-sed -i "s/PUBLIC_IP_1/$PUBLIC_IP/g" ../scripts/login.sh
